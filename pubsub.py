0fdd62f22ec3428463773ad9d9807f5829830db3
Vivek <vivekpn.93@gmail.com>
2017-06-05 01:10:23 +0000

    calling pubsub from ultrasonic

--- pubsub.py
+++ pubsub.py
@@ -5,10 +5,10 @@ import sys
 import logging
 import time
 import getopt
-from datetime import datetime
 import json
 import weather
 import traffic
+from datetime import datetime
 
 # Custom MQTT message callback
 
@@ -20,9 +20,9 @@ def customCallback(client, userdata, message):
 	print("--------------\n\n")
 
 
-def get_pi_data():
+def get_pi_data(peoplecount):
 	data = {}
-	data['people_count'] = 0
+	data['people_count'] = peoplecount
 	return data
 
 
@@ -38,118 +38,126 @@ def get_calendar_data():
 	return data;
 
 
-# Usage
-usageInfo = """Usage:
+def main(host, privateKeyPath,certificatePath, rootCAPath, useWebsocket, peoplecount):
+	# Configure logging
+	logger = logging.getLogger("AWSIoTPythonSDK.core")
+	logger.setLevel(logging.DEBUG)
+	streamHandler = logging.StreamHandler(sys.stdout)
+	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+	streamHandler.setFormatter(formatter)
+	logger.addHandler(streamHandler)
 
-Use certificate based mutual authentication:
-python basicPubSub.py -e <endpoint> -r <rootCAFilePath> -c <certFilePath> -k <privateKeyFilePath>
+	# Init AWSIoTMQTTClient
+	myAWSIoTMQTTClient = None
+	if useWebsocket:
+		myAWSIoTMQTTClient = AWSIoTMQTTClient("basicPubSub", useWebsocket=True)
+		myAWSIoTMQTTClient.configureEndpoint(host, 443)
+		myAWSIoTMQTTClient.configureCredentials(rootCAPath)
+	else:
+		myAWSIoTMQTTClient = AWSIoTMQTTClient("basicPubSub")
+		myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+		myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
 
-Use MQTT over WebSocket:
-python basicPubSub.py -e <endpoint> -r <rootCAFilePath> -w
+	# AWSIoTMQTTClient connection configuration
+	myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+	myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+	myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+	myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+	myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
-Type "python basicPubSub.py -h" for available options.
-"""
-# Help info
-helpInfo = """-e, --endpoint
-	Your AWS IoT custom endpoint
--r, --rootCA
-	Root CA file path
--c, --cert
-	Certificate file path
--k, --key
-	Private key file path
--w, --websocket
-	Use MQTT over WebSocket
--h, --help
-	Help information
+	# Connect and subscribe to AWS IoT
+	myAWSIoTMQTTClient.connect()
+	TOPIC = "sdk/test/Python"
+	# myAWSIoTMQTTClient.subscribe(TOPIC, 1, customCallback)
+	# time.sleep(2)
+
+	data = {}
+	data['time'] = get_calendar_data()
+	data['pi'] = get_pi_data(peoplecount)
+	data['weather'] = weather.get_weather_data()
+	data['traffic'] = traffic.get_traffic_data()
+
+	myAWSIoTMQTTClient.publish(TOPIC, json.dumps(data), 1)
+
+	myAWSIoTMQTTClient.disconnect()
+
+if __name__ == '__main__': 
+	# Usage
+	usageInfo = """Usage:
+
+	Use certificate based mutual authentication:
+	python basicPubSub.py -e <endpoint> -r <rootCAFilePath> -c <certFilePath> -k <privateKeyFilePath>
+
+	Use MQTT over WebSocket:
+	python basicPubSub.py -e <endpoint> -r <rootCAFilePath> -w
+
+	Type "python basicPubSub.py -h" for available options.
+	"""
+	# Help info
+	helpInfo = """-e, --endpoint
+		Your AWS IoT custom endpoint
+	-r, --rootCA
+		Root CA file path
+	-c, --cert
+		Certificate file path
+	-k, --key
+		Private key file path
+	-w, --websocket
+		Use MQTT over WebSocket
+	-h, --help
+		Help information
 
 
-"""
+	"""
 
-# Read in command-line parameters
-useWebsocket = False
-host = ""
-rootCAPath = ""
-certificatePath = ""
-privateKeyPath = ""
-try:
-	opts, args = getopt.getopt(sys.argv[1:], "hwe:k:c:r:", ["help", "endpoint=", "key=","cert=","rootCA=", "websocket"])
-	if len(opts) == 0:
-		raise getopt.GetoptError("No input parameters!")
-	for opt, arg in opts:
-		if opt in ("-h", "--help"):
-			print(helpInfo)
-			exit(0)
-		if opt in ("-e", "--endpoint"):
-			host = arg
-		if opt in ("-r", "--rootCA"):
-			rootCAPath = arg
-		if opt in ("-c", "--cert"):
-			certificatePath = arg
-		if opt in ("-k", "--key"):
-			privateKeyPath = arg
-		if opt in ("-w", "--websocket"):
-			useWebsocket = True
-except getopt.GetoptError:
-	print(usageInfo)
-	exit(1)
+	# Read in command-line parameters
+	useWebsocket = False
+	host = ""
+	rootCAPath = ""
+	certificatePath = ""
+	privateKeyPath = ""
+	peoplecount = 0
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "hwe:k:c:r:p:", ["help", "endpoint=", "key=","cert=","rootCA=", "websocket", "pi="])
+		if len(opts) == 0:
+			raise getopt.GetoptError("No input parameters!")
+		for opt, arg in opts:
+			if opt in ("-h", "--help"):
+				print(helpInfo)
+				exit(0)
+			if opt in ("-e", "--endpoint"):
+				host = arg
+			if opt in ("-r", "--rootCA"):
+				rootCAPath = arg
+			if opt in ("-c", "--cert"):
+				certificatePath = arg
+			if opt in ("-k", "--key"):
+				privateKeyPath = arg
+			if opt in ("-w", "--websocket"):
+				useWebsocket = True
+			if opt in ("-pi"):
+				peoplecount = int(arg)
+	except getopt.GetoptError:
+		print(usageInfo)
+		exit(1)
 
-# Missing configuration notification
-missingConfiguration = False
-if not host:
-	print("Missing '-e' or '--endpoint'")
-	missingConfiguration = True
-if not rootCAPath:
-	print("Missing '-r' or '--rootCA'")
-	missingConfiguration = True
-if not useWebsocket:
-	if not certificatePath:
-		print("Missing '-c' or '--cert'")
+	# Missing configuration notification
+	missingConfiguration = False
+	if not host:
+		print("Missing '-e' or '--endpoint'")
 		missingConfiguration = True
-	if not privateKeyPath:
-		print("Missing '-k' or '--key'")
+	if not rootCAPath:
+		print("Missing '-r' or '--rootCA'")
 		missingConfiguration = True
-if missingConfiguration:
-	exit(2)
+	if not useWebsocket:
+		if not certificatePath:
+			print("Missing '-c' or '--cert'")
+			missingConfiguration = True
+		if not privateKeyPath:
+			print("Missing '-k' or '--key'")
+			missingConfiguration = True
+	if missingConfiguration:
+		exit(2)
 
-# Configure logging
-logger = logging.getLogger("AWSIoTPythonSDK.core")
-logger.setLevel(logging.DEBUG)
-streamHandler = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-streamHandler.setFormatter(formatter)
-logger.addHandler(streamHandler)
+	main(host, privateKeyPath,certificatePath, rootCAPath, useWebsocket, peoplecount)
 
-# Init AWSIoTMQTTClient
-myAWSIoTMQTTClient = None
-if useWebsocket:
-	myAWSIoTMQTTClient = AWSIoTMQTTClient("basicPubSub", useWebsocket=True)
-	myAWSIoTMQTTClient.configureEndpoint(host, 443)
-	myAWSIoTMQTTClient.configureCredentials(rootCAPath)
-else:
-	myAWSIoTMQTTClient = AWSIoTMQTTClient("basicPubSub")
-	myAWSIoTMQTTClient.configureEndpoint(host, 8883)
-	myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
-
-# AWSIoTMQTTClient connection configuration
-myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
-myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
-myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
-
-# Connect and subscribe to AWS IoT
-myAWSIoTMQTTClient.connect()
-TOPIC = "sdk/test/Python"
-# myAWSIoTMQTTClient.subscribe(TOPIC, 1, customCallback)
-# time.sleep(2)
-
-data = {}
-data['time'] = get_calendar_data()
-data['pi'] = get_pi_data()
-data['weather'] = weather.get_weather_data()
-data['traffic'] = traffic.get_traffic_data()
-
-myAWSIoTMQTTClient.publish(TOPIC, json.dumps(data), 1)
-
-myAWSIoTMQTTClient.disconnect()
--- ultrasonic1.py
+++ ultrasonic1.py
@@ -4,6 +4,85 @@ import sys
 import time
 import RPi.GPIO as GPIO
 from display import TM1637
+import  pubsub
+import getopt
+
+if __name__ == '__main__': 
+	# Usage
+	usageInfo = """Usage:
+
+	Use certificate based mutual authentication:
+	python basicPubSub.py -e <endpoint> -r <rootCAFilePath> -c <certFilePath> -k <privateKeyFilePath>
+
+	Use MQTT over WebSocket:
+	python basicPubSub.py -e <endpoint> -r <rootCAFilePath> -w
+
+	Type "python basicPubSub.py -h" for available options.
+	"""
+	# Help info
+	helpInfo = """-e, --endpoint
+		Your AWS IoT custom endpoint
+	-r, --rootCA
+		Root CA file path
+	-c, --cert
+		Certificate file path
+	-k, --key
+		Private key file path
+	-w, --websocket
+		Use MQTT over WebSocket
+	-h, --help
+		Help information
+
+
+	"""
+
+	# Read in command-line parameters
+	useWebsocket = False
+	host = ""
+	rootCAPath = ""
+	certificatePath = ""
+	privateKeyPath = ""
+	peoplecount = 0
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "hwe:k:c:r:", ["help", "endpoint=", "key=","cert=","rootCA=", "websocket"])
+		if len(opts) == 0:
+			raise getopt.GetoptError("No input parameters!")
+		for opt, arg in opts:
+			if opt in ("-h", "--help"):
+				print(helpInfo)
+				exit(0)
+			if opt in ("-e", "--endpoint"):
+				host = arg
+			if opt in ("-r", "--rootCA"):
+				rootCAPath = arg
+			if opt in ("-c", "--cert"):
+				certificatePath = arg
+			if opt in ("-k", "--key"):
+				privateKeyPath = arg
+			if opt in ("-w", "--websocket"):
+				useWebsocket = True
+	except getopt.GetoptError:
+		print(usageInfo)
+		exit(1)
+
+	# Missing configuration notification
+	missingConfiguration = False
+	if not host:
+		print("Missing '-e' or '--endpoint'")
+		missingConfiguration = True
+	if not rootCAPath:
+		print("Missing '-r' or '--rootCA'")
+		missingConfiguration = True
+	if not useWebsocket:
+		if not certificatePath:
+			print("Missing '-c' or '--cert'")
+			missingConfiguration = True
+		if not privateKeyPath:
+			print("Missing '-k' or '--key'")
+			missingConfiguration = True
+	if missingConfiguration:
+		exit(2)
+
 
 THRESHOULD1 = 80
 THRESHOULD2 = 80
@@ -26,11 +105,11 @@ GPIO.setup(GPIO_ECHO1,GPIO.IN)      # Echo
 GPIO.setup(GPIO_ECHO2,GPIO.IN)      # Echo
 
 
-def signal_handler(signal, frame):
+def interrupt_handler(signal, frame):
     GPIO.cleanup()
     sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGINT, interrupt_handler)
 
 
 #Variables for people count
@@ -48,10 +127,16 @@ display.Show(digits)
 time.sleep(3)
 
 display.Clear()
-
+PUBLISHING_TIME = 2
 #Init complete
+starttime=time.time()
+people = 0
 while True:
-  
+  if (time.time() - starttime) > (60.0 * PUBLISHING_TIME):
+    starttime = time.time()
+    pubsub.main(host, privateKeyPath,certificatePath, rootCAPath, useWebsocket, people)
+    print "published to dynamodb"
+
   # Set trigger to False (Low)
   GPIO.output(GPIO_TRIGGER1, False)
 
@@ -156,7 +241,6 @@ while True:
   #print "people = %d "%people
   display.ShowInt(people)
  
-
-#End of measurements
+# End of measurements
 # Reset GPIO settings
 GPIO.cleanup()
